128c0e951e8440595277ee3c1f9ea8da6f3a2550
risexegebis165-afk <risexegebis165@gmail.com>
2026-02-21 23:04:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,21 @@ app.get("/", (req,res)=>{
   res.send("YT Server Running 🚀");
 });
 
-// 🔹 Video Info (Title + Thumbnail + MP3 formats)
+// 🔹 Get All Formats
 app.get("/info", async (req,res)=>{
   try{
     const info = await ytdl.getInfo(req.query.url);
 
-    const audioFormats = ytdl.filterFormats(info.formats, "audioonly")
-      .map(f => ({
+    const videoFormats = ytdl.filterFormats(info.formats,"videoandaudio")
+      .map(f=>({
+        itag: f.itag,
+        quality: f.qualityLabel,
+        container: f.container
+      }))
+      .filter(f=>f.quality);
+
+    const audioFormats = ytdl.filterFormats(info.formats,"audioonly")
+      .map(f=>({
         itag: f.itag,
         bitrate: f.audioBitrate + " kbps",
         container: f.container
@@ -24,6 +32,7 @@ app.get("/info", async (req,res)=>{
     res.json({
       title: info.videoDetails.title,
       thumbnail: info.videoDetails.thumbnails.pop().url,
+      videos: videoFormats,
       audios: audioFormats
     });
 
@@ -32,14 +41,18 @@ app.get("/info", async (req,res)=>{
   }
 });
 
-// 🔹 MP3 Download (Selected Quality)
-app.get("/mp3", (req,res)=>{
-  const url = req.query.url;
-  const itag = req.query.itag;
+// 🔹 MP4 Download
+app.get("/mp4",(req,res)=>{
+  const {url, itag} = req.query;
+  res.header("Content-Disposition","attachment; filename=video.mp4");
+  ytdl(url,{quality:itag}).pipe(res);
+});
 
+// 🔹 MP3 Download
+app.get("/mp3",(req,res)=>{
+  const {url, itag} = req.query;
   res.header("Content-Disposition","attachment; filename=audio.mp3");
-
-  ytdl(url,{ quality: itag, filter:"audioonly" }).pipe(res);
+  ytdl(url,{quality:itag, filter:"audioonly"}).pipe(res);
 });
 
 const PORT = process.env.PORT || 3000;
